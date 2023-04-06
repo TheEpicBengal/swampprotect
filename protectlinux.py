de82a9014d54c063ccac75464667b4f95d076576
TheEpicBengal <109075427+TheEpicBengal@users.noreply.github.com>
2023-04-06 16:46:16 -0500

    Update protectlinux.py

--- protectlinux.py
+++ protectlinux.py
@@ -5,7 +5,7 @@ import pyautogui
 while True:
         print("Auto Protecting soon....")
         time.sleep(20)
-        pyautogui.press('-') # Simulate pressing the key that is binded in this case it's -'
+        pyautogui.press('-') # You can choose your own key or type bind - "say /protect" in gmod console
         print("Protection Activated, Time until next protection sequence countdown:")
         for i in range(4):
                 t = (1150)
