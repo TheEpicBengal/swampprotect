b09521dab6aa6f3a8f88b58c436ecd7295a4669b
TheEpicBengal <109075427+TheEpicBengal@users.noreply.github.com>
2023-04-06 16:46:47 -0500

    Update protect.py

--- protect.py
+++ protect.py
@@ -15,6 +15,6 @@ while True:
                 mins = t // 60 
                 secs = t % 60
                 timer = '{:02d}:{:02d}'.format(mins, secs) 
-                print(timer, end="\r") # overwrite previous line 
+                print(timer, end="\r")
                 time.sleep(1)
                 t -= 1  
